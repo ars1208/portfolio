f5b0260c9aaaee29a3ab07a8ac601e3e13b50da6
ars1208 <ara.kaz1208@gmail.com>
2020-02-07 13:59:02 +0900

    Initial commit

---
+++ src/components/page1.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>
+      ページ1です。
+    </p>
+    <router-link to="/page2">page2へ</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/components/page2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>
+      ページ2です。
+    </p>
+    <router-link to="/">page1へ</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
