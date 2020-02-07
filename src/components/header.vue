d00013b1ab01f95075d463a0f1049066aa273cc8
ars1208 <ara.kaz1208@gmail.com>
2020-02-07 14:19:38 +0900

    Initial commit

---
+++ src/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <div class="header-logo">
+        My logo
+      </div>
+      <div class="header-list">
+        <ul>
+          <li>Top</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Contacts</li>
+        </ul>
+      </div>
+    </header>
+    <router-link to="/page2">page2へ</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import page1 from '@/components/page1'
+import header from '@/components/header'
 import page2 from '@/components/page2'
 
 Vue.use(Router)
@@ -10,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: page1
+      component: header
     },
     {
       path: '/page2',
