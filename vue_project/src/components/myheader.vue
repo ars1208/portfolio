c6f43db8f55d42988949f32ff93ccc5a350acd6f
ars1208 <ara.kaz1208@gmail.com>
2020-02-10 04:22:42 +0900

    add header

--- vue_project/src/components/myheader.vue
+++ vue_project/src/components/myheader.vue
@@ -4,16 +4,15 @@
       <div class="header-logo">
         My logo
       </div>
-      <div class="header-list">
+      <nav class="header-list">
         <ul>
-          <li>Top</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Contacts</li>
+          <li><a href="#">Top</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Skills</a></li>
+          <li><a href="#">Contacts</a></li>
         </ul>
-      </div>
+      </nav>
     </header>
-    <router-link to="/page2">page2へ</router-link>
   </div>
 </template>
 
@@ -21,5 +20,46 @@
 </script>
 
 <style>
+header {
+  height: 70px;
+  width: 100%;
+}
+
+.header-logo {
+  display: inline-block;
+  float: left;
+  line-height: 70px;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.header-list {
+  float: right;
+  height: 70px;
+}
+
+.header-list ul {
+  list-style: none;
+  display: inline-block;
+  height: 70px;
+  margin: 0;
+  padding: 0;
+}
+
+.header-list li {
+  float: left;
+}
+
+.header-list a {
+  color: inherit;
+  display: block;
+  padding: 20px 40px;
+  margin: 3px 10px;
+  text-decoration: none;
+}
+
+.header-list a:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
 
 </style>
