6d1bb210cb2e277587d84520917a34b9c4b99d2d
ars1208 <ara.kaz1208@gmail.com>
2020-04-20 08:26:13 +0900

    add aboutme

--- vue_project/src/App.vue
+++ vue_project/src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <myheader></myheader>
     <toppage></toppage>
+    <aboutme></aboutme>
   </div>
 </template>
 
 <script>
 import myheader from './components/myheader'
 import toppage from './components/topPage'
+import aboutme from './components/aboutMe'
 
 export default {
   components: {
     myheader,
-    toppage
+    toppage,
+    aboutme
   }
 }
 </script>
---
+++ vue_project/src/components/aboutMe.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="about">
+      <p>top</p>
+    </div>
+  </div>
+</template>
+
+<style>
+  #about p {
+    background-color: #00bf00;
+    height: 500px;
+  }
+</style>
