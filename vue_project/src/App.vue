4c7f50c105ca53913db25119ce772fdbcf88e3a3
ars1208 <ara.kaz1208@gmail.com>
2020-04-19 16:45:19 +0900

    add toppage

--- vue_project/src/App.vue
+++ vue_project/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <myheader></myheader>
+    <toppage></toppage>
   </div>
 </template>
 
 <script>
 import myheader from './components/myheader'
+import toppage from './components/topPage'
 
 export default {
   components: {
-    myheader
+    myheader,
+    toppage
   }
 }
 </script>
---
+++ vue_project/src/components/topPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="top">
+      <p>top</p>
+    </div>
+  </div>
+</template>
+
+<style>
+  p {
+    background-color: #00bfff;
+    height: 500px;
+  }
+</style>
