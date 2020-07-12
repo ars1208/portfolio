60c8b86ac58be5529f53d022a62fd4093dc80c98
ars1208 <ara.kaz1208@gmail.com>
2020-07-12 10:14:43 +0900

    update contacts

--- vue_project/src/components/contactMe.vue
+++ vue_project/src/components/contactMe.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <div class="contact">
-      <p>contacts</p>
+      <ul>
+        <li>Gmail</li>
+        <li>Twitter</li>
+        <li>Linkedin</li>
+        <li>GitHub</li>
+      </ul>
     </div>
   </div>
 </template>
